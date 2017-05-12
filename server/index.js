be0c53e6b788d73dca0be31b1c4051ab69e512c0
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-12 14:40:27 -0700

    comment out headers

--- server/index.js
+++ server/index.js
@@ -27,12 +27,12 @@ io.on('connection', socket => {
 });
 
 // if (process.env.NODE_ENV === 'development') {
-  app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
-    res.header("Access-Control-Allow-Methods", 'GET, POST, PUT ,DELETE');
-    next();
-  });
+  // app.all('/*', function (req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  //   res.header("Access-Control-Allow-Methods", 'GET, POST, PUT ,DELETE');
+  //   next();
+  // });
 // }
 
 app.use(morgan());
