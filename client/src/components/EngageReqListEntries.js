103dc31721880ac53c8fe7b796f90915ab58e308
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-13 02:31:53 -0700

    more options on peerjs

--- client/src/components/EngageReqListEntries.js
+++ client/src/components/EngageReqListEntries.js
@@ -98,7 +98,7 @@ class EngageReqListEntries extends Component {
   sendPeerId() {
     // const socket = io('http://localhost:5000');
     const socket = io('https://powerful-island-56927.herokuapp.com');
-    const peer = new Peer({key: PEERS_API_KEY, secure: true});
+    const peer = new Peer({key: PEERS_API_KEY, secure: true, port:443, key: 'peerjs'});
 
     console.log('IN SEED PEER ID', peer);
 
